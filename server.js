5380f6f17360b850e9856f7b321d72d82998052e
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-03-18 22:22:49 -0500

    updated files

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = process.env.PORT ||3000;
+const Schema = mongoose.Schema;
 
-const app = express();
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for transaction"
+    },
+    value: {
+      type: Number,
+      required: "Enter an amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
 
-app.use(logger("dev"));
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-mongoose.connect(
-  process.env.MONGODB_URI || "'mongodb://localhost/budget",
- {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+module.exports = Transaction;
